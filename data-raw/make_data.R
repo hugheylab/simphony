9078a0b34ea66f585bf12ca00659de11b2540ce3
Jake Hughey <jakejhughey@gmail.com>
2018-12-12 13:53:57 -0600

    removed unnecessary objects from the environment of defaultDispFunc. removing anything large from the locfit object rendered it unusable.

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,3 +1,20 @@
 dispFuncs = readRDS('../../projects/simphony_analysis/results/mouse_disp_funcs.rds')
 defaultDispFunc = dispFuncs$local
+
+##### cut a little fat
+env = environment(defaultDispFunc)
+
+ls(env)
+rm('means', 'disps', 'd', 'minDisp', envir = env)
+
+# names(env$fit)
+# env$fit$frame = NULL
+# failed: eva, cell, terms, nvc, mi, , trans
+# ok to remove: box, sty, deriv,
+
 usethis::use_data(defaultDispFunc, internal = FALSE, overwrite = TRUE)
+
+##### check that it still works
+
+load('data/defaultDispFunc.rda')
+defaultDispFunc(2^(0:20))
